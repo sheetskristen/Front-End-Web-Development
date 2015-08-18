f7d441e3cf7a2792b964f3e22465387d08a76559
sheetskristen <sheetskristen@gmail.com>
2015-08-17 21:52:50 -0700

    All done

--- Class 4/script.js
+++ Class 4/script.js
@@ -5,8 +5,18 @@ var sum = addend1 + addend2;
  
 console.log(sum); 
  
-alert("The sum is "+ sum); 
+/* alert("The sum is "+ sum); 
   
 var isItFifteen = confirm("The sum is 15??");
  
-sum = prompt("What is the sum?"); 
+sum = prompt("What is the sum?"); */
+
+var age = prompt("How old are you?");
+
+age = parseInt(age); 
+
+if(isNaN(age)) {
+	alert("You screwed up!");
+} else if (age < 200){
+	alert("You're so young!");
+}

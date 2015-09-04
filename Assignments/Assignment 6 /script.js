e1b75ffa25c826e9474b41ff99ace4c45b7abefe
sheetskristen <sheetskristen@gmail.com>
2015-09-03 22:35:41 -0700

    adding assignment 6

---
+++ Assignments/Assignment 6 /script.js
@@ -0,0 +1,30 @@
+var toDoList = $('.to-do-list'); 
+var addButton = $('.add'); 
+var newField = $('.new-field');
+
+(addButton).click(function(){
+// 	var newItem = prompt("Add a new to do item:");
+	var newItem = newField.val(); 
+	
+	var newToDo = toDoList.find("li").first().clone();
+	
+	newToDo.text(newItem);
+	
+	(toDoList).append(newToDo);
+	
+
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
